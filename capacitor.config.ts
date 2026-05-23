d94caf51f80db6965e2c2a5eefa5bb4528e91777
taein0707 <taein0707@naver.com>
2026-05-24 02:54:35 +0900

    Fix Codemagic iOS workflow

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ocl.app',
+  appName: 'OCL',
+  webDir: 'dist',
+};
+
+export default config;
