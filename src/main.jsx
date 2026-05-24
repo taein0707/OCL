c7d8f4f3ffae92300b4b07ab8c8bd2e3ea4361ee
taein0707 <taein0707@naver.com>
2026-05-24 17:11:30 +0900

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,4 @@
 import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-
 import App from './App.jsx'
-import ErrorBoundary from './components/ErrorBoundary.jsx'
 
-const rootElement = document.getElementById('root')
-
-if (!rootElement) {
-  throw new Error('Root element not found')
-}
-
-createRoot(rootElement).render(
-  <ErrorBoundary>
-    <App />
-  </ErrorBoundary>
-)
+createRoot(document.getElementById('root')).render(<App />)
